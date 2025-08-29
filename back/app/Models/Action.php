71373dfa4e86465ba325d1f7009101b4472bec49
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-08-30 02:44:10 +0330

    Update Action.php

--- back/app/Models/Action.php
+++ back/app/Models/Action.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Action extends Model
 {
@@ -15,18 +18,23 @@ class Action extends Model
         'release' => 'datetime',
     ];
 
-    public function missions()
+    public function missions(): HasMany
     {
         return $this->hasMany(Mission::class);
     }
 
-    public function dependency()
+    public function dependency(): HasMany
     {
         return $this->hasMany(ActionDependency::class);
     }
 
-    public function region()
+    public function region(): BelongsTo
     {
-        return $this->belongsTo(\App\Models\Region::class, 'region_id');
+        return $this->belongsTo(Region::class, 'region_id');
+    }
+
+    public function teams(): BelongsToMany
+    {
+        return $this->belongsToMany(Team::class, 'action_team');
     }
 }
