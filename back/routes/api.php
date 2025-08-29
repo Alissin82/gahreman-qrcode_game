1f6169d7528534be03d6619dbc71c52061ce1283
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-08-30 01:00:32 +0330

    remove unnecessary routes

--- back/routes/api.php
+++ back/routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ActionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\TeamController;
 
 Route::prefix('teams')->name('teams.')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
@@ -15,9 +14,3 @@ Route::prefix('teams')->name('teams.')->group(function () {
 
 Route::get('/actions', [ActionController::class, 'index']);
 Route::get('/actions/{id}', [ActionController::class, 'show']);
-
-Route::get('/teams', [TeamController::class, 'index']);
-Route::get('/teams/{id}', [TeamController::class, 'show']);
-Route::post('/teams', [TeamController::class, 'store']);
-Route::put('/teams/{id}', [TeamController::class, 'update']);
-Route::delete('/teams/{id}', [TeamController::class, 'destroy']);
