a4b0e44cb2c3a378f7ea54d9ab8f7c5d8bdaef1a
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-08-30 04:13:56 +0330

    Update DatabaseSeeder.php

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Action;
+use App\Models\Mission;
+use App\Models\Region;
+use App\Models\Team;
+use App\Models\TeamAdmins;
+use App\Models\TeamUsers;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
 
@@ -21,5 +27,41 @@ class DatabaseSeeder extends Seeder
             AdminUserSeeder::class,
         ]);
 
+        if (config('app.env') === 'local') {
+            $team = Team::create([
+                'name' => fake()->name,
+                'color' => "#ff0000",
+                'bio' => "ما می‌توانیم",
+                'score' => 0,
+                'coin' => 0,
+                'phone' => "09138884444",
+                'hash' => "1234567812345678",
+                'gender' => fake()->boolean,
+            ]);
+            TeamUsers::create([
+                'team_id' => $team->id,
+                'name' => fake()->firstName,
+                'family' => fake()->lastName,
+            ]);
+            TeamAdmins::create([
+                'team_id' => $team->id,
+                'name' => fake()->firstName,
+                'family' => fake()->lastName,
+            ]);
+
+            $region = Region::create([
+               'name' => fake()->word,
+            ]);
+            $action = Action::create([
+                'name' => fake()->word(),
+                'region_id' => $region->id,
+                'release' => now()
+            ]);
+            Mission::create([
+                'action_id' => $action->id,
+                'title' => fake()->word(),
+                'score' => 0,
+            ]);
+        }
     }
 }
