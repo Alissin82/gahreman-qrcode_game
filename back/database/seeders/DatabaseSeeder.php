63ca44955b6de8b8a4771e46852dc441b1a49d8f
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-08-30 15:12:18 +0330

    feat import old data via excel

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Imports\OldDataImport;
 use App\Models\Action;
 use App\Models\Coin;
 use App\Models\Mission;
@@ -12,6 +13,9 @@ use App\Models\TeamAdmins;
 use App\Models\TeamUsers;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+use Morilog\Jalali\Jalalian;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,27 +34,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         if (config('app.env') === 'local') {
-            $team = Team::create([
-                'name' => fake()->name,
-                'color' => "#ff0000",
-                'bio' => "ما می‌توانیم",
-                'score' => 0,
-                'coin' => 0,
-                'phone' => "09138884444",
-                'hash' => "1234567812345678",
-                'gender' => fake()->boolean,
-            ]);
-            TeamUsers::create([
-                'team_id' => $team->id,
-                'name' => fake()->firstName,
-                'family' => fake()->lastName,
-            ]);
-            TeamAdmins::create([
-                'team_id' => $team->id,
-                'name' => fake()->firstName,
-                'family' => fake()->lastName,
-            ]);
-
             $region = Region::create([
                'name' => fake()->word,
             ]);
@@ -74,6 +57,77 @@ class DatabaseSeeder extends Seeder
                 'name' => "کارت 200 امتیازی",
                 'score' => 200,
             ]);
+        }
+
+        // تاریخ شروع برای داده‌هایی که قرار است وارد شوند
+        $startDate = (new Jalalian(1404, 2, 11))->toCarbon();
+
+        // مربی پیش فرض مرد
+        $boysTeamAdmin = [
+            'name' => " مهدی",
+            'family' => 'جوانمردی',
+            'gender' => true,
+            'start' => $startDate
+        ];
+
+        // مربی پیش فرض خانم
+        $girlsTeamAdmin = [
+            'name' => "اعظم",
+            'family' => 'بلکامه',
+            'gender' => false,
+            'start' => $startDate
+        ];
+
+        $excels = [];
+        $excels[] = [
+            'path' => Storage::disk('local')->path('old-data/boys.xlsx'),
+            'gender' => true, // for male
+            'admin' => $boysTeamAdmin,
+        ];
+        $excels[] = [
+            'path' => Storage::disk('local')->path('old-data/girls.xlsx'),
+            'gender' => false, // for females
+            'admin' => $girlsTeamAdmin,
+        ];
+        foreach ($excels as $excel) {
+            $boysSheets = Excel::toCollection(new OldDataImport(), $excel['path']);
+            foreach ($boysSheets as $sheet) {
+                // هر sheet یک تیم هست
+
+                $sheetArray = $sheet->toArray();
+                array_shift($sheetArray); // just to surpass title
+                array_shift($sheetArray); // just to surpass headers
+                $firstRow = $sheetArray[0];
+                // گروهان
+                $companyName = $firstRow[3];
+                // تعداد پویندگان
+                $followersCount = $firstRow[4];
+
+                $team = Team::create([
+                    'name' => $companyName,
+                    'gender' => $excel['gender'],
+                ]);
+
+                TeamAdmins::create(array_merge($excel['admin'], [
+                    'team_id' => $team->id,
+                ]));
+
+                $teamUsers = [];
+                foreach ($sheetArray as $row) {
+                    $now = now();
+                    $teamUsers[] = [
+                        'name' => $row[2],
+                        'family' => '',
+                        'team_id' => $team->id,
+                        'city' => $row[0],
+                        'province' => 'اصفهان',
+                        'gender' => $excel['gender'],
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ];
+                }
+                TeamUsers::insert($teamUsers);
+            }
         }
     }
 }
