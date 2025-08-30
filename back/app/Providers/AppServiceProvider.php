fbaa55f0b4987e4e55f38d5fc7e79d38ff972ec7
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-08-30 15:40:14 +0330

    *

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Octane\Facades\Octane;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_DEBUG') == false ) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
