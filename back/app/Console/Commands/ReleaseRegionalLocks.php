ca93b25dd5b697658953dfe63505f1b03adb4fa5
Ali <ghostrockz3d@gmail.com>
2025-08-31 22:21:43 +0330

    work on releasing locks

---
+++ back/app/Console/Commands/ReleaseRegionalLocks.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\ActionStatus;
+use App\Models\ActionTeam;
+use App\Models\Region;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class ReleaseRegionalLocks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:release-regional-locks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Release regional locks';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Region::where('lockable', true)->chunkById(100, function ($regions) {
+            $regions->each(function ($region) {
+                $hasPending = ActionTeam::with('action')->where('region_id', $region->id)->where('status', ActionStatus::Pending)->get();
+
+                if ($hasPending->isEmpty()) {
+                    $region->lockable = false;
+                    $region->save();
+                }
+
+                $hasPending->each(function (ActionTeam $actionTeam) use ($region) {
+                    if (now() > $actionTeam->created_at->addMinutes($actionTeam->action->duration)) {
+                        $actionTeam->status = ActionStatus::Completed;
+                        $actionTeam->save();
+
+                        $region->lockable = false;
+                        $region->save();
+                    }
+                });
+            });
+        });
+    }
+}
