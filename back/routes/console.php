7682cb17dcbb9ffa1b9b2f419872ecf302665503
Ali <ghostrockz3d@gmail.com>
2025-08-31 22:49:16 +0330

    fix schedular

--- back/routes/console.php
+++ back/routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::call(new ReleaseRegionalLocks)->everyMinute();
+Schedule::command(ReleaseRegionalLocks::class)->everyMinute();
