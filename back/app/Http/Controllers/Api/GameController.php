168af65e3d94ecb99fca4d5a1003d76d4add0657
Ali Alizadeh <ghostrockz3d@gmail.com>
2025-09-01 15:35:01 +0330

    wip

--- back/app/Http/Controllers/Api/GameController.php
+++ back/app/Http/Controllers/Api/GameController.php
@@ -10,6 +10,7 @@ use App\Http\Resources\GameResource;
 use App\Models\ScoreTeam;
 use Illuminate\Support\Facades\Auth;
 use App\Models\GameTeam;
+use App\Models\ScoreCard;
 
 class GameController extends Controller
 {
