fa03d354560aea4ef3d4cf1c2679044f371a1bf2
Ali Mashayekhi <alim201381@gmail.com>
2025-09-02 10:20:52 +0330

    fix: task ordering

--- back/Modules/Task/app/Observers/TaskObserver.php
+++ back/Modules/Task/app/Observers/TaskObserver.php
@@ -12,13 +12,12 @@ class TaskObserver
 {
     public function saving(Task $task): void
     {
-
         $type = match ($task->taskable_type) {
             MCQ::class => TaskType::MCQ,
             FileUpload::class => TaskType::UploadFile
         };
         $task->type = $type;
-        $hasOrder = Task::whereActionId($task->id)->exists();
+        $hasOrder = Task::whereActionId($task->action_id)->exists();
         if ($hasOrder) {
             $task->order = Task::whereActionId($task->action_id)->max('order') + 1;
         } else $task->order = 0;
