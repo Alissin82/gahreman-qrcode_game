90e2f5c45daa9d4855944ca0c260bd70c4641379
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-09-03 00:07:13 +0330

    Update AdminUserSeeder.php

--- back/database/seeders/AdminUserSeeder.php
+++ back/database/seeders/AdminUserSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class AdminUserSeeder extends Seeder
@@ -15,39 +14,54 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        $super = User::updateOrCreate(
-            [
-                'email' => 'super@iraniom.ir',
-            ],
-            [
-                'name' => 'مدیریت',
-                'phone' => '09120000000',
-                'password' => Hash::make('password'),
-            ]
-        );
-
-        $admin = User::updateOrCreate(
-            [
-                'email' => 'admin@iraniom.ir',
-            ],
-            [
-                'name' => 'داور',
-                'phone' => '09130000000',
-                'password' => Hash::make('password'),
-            ]
-        );
+        // Super Admin
         Role::firstOrCreate(['name' => 'super_admin']);
-        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        User::firstOrCreate([
+            'email' => 'super@iraniom.ir',
+            'name' => 'مدیر',
+            'phone' => '09120000000',
+            'password' => Hash::make('password'),
+        ]);
+        User::where('email', 'super@iraniom.ir')->firstOrFail()->assignRole('super_admin');
 
-        $extraPermissions = [
-            ...Permission::where('name', 'like', '%extra%')->pluck('name')->toArray(),
-            ...Permission::where('name', 'like', '%review%')->pluck('name')->toArray(),
-            ...Permission::where('name', 'like', '%team_coin%')->pluck('name')->toArray(),
+        // Team Admin
+        $teamAdminPermissions = [
+            // Team اصلی
+            'view_team',
+            'view_any_team',
+            'create_team',
+            'update_team',
+            'restore_team',
+            'restore_any_team',
+            'replicate_team',
+            'reorder_team',
+            'delete_team',
+            'delete_any_team',
+            'force_delete_team',
+            'force_delete_any_team',
+
+            // TeamUsers
+            'view_team::users',
+            'view_any_team::users',
+            'create_team::users',
+            'update_team::users',
+            'restore_team::users',
+            'restore_any_team::users',
+            'replicate_team::users',
+            'reorder_team::users',
+            'delete_team::users',
+            'delete_any_team::users',
+            'force_delete_team::users',
+            'force_delete_any_team::users',
         ];
-        $adminRole->syncPermissions($extraPermissions);
-
-
-        $admin->assignRole('admin');
-        $super->assignRole('super_admin');
+        Role::firstOrCreate(['name' => 'team_admin']);
+        Role::where('name', 'team_admin')->firstOrFail()->syncPermissions($teamAdminPermissions);
+        User::firstOrCreate([
+            'email' => 'team@iraniom.ir',
+            'name' => 'مدیر تیم',
+            'phone' => '09130000000',
+            'password' => Hash::make('password'),
+        ]);
+        User::where('email', 'team@iraniom.ir')->firstOrFail()->assignRole('team_admin');
     }
 }
