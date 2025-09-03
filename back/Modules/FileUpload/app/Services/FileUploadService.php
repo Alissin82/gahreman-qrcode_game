b336d41045420584e8c8b3f3039556acd9eaebad
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-09-03 12:57:35 +0330

    Update FileUploadService.php

--- back/Modules/FileUpload/app/Services/FileUploadService.php
+++ back/Modules/FileUpload/app/Services/FileUploadService.php
@@ -4,25 +4,30 @@ namespace Modules\FileUpload\Services;
 
 use App\Models\ScoreTeam;
 use App\Models\Team;
+use DB;
 use Modules\FileUpload\Models\FileUpload;
 use Modules\FileUpload\Models\FileUploadTeam;
 use Modules\Task\Exceptions\TaskAlreadyDoneException;
 use Modules\Task\Models\Task;
+use Throwable;
 
 class FileUploadService
 {
     /**
-     * @throws TaskAlreadyDoneException
+     * @throws TaskAlreadyDoneException|Throwable
      */
-    public function answer(Team $team, FileUpload $fileUpload, array $data): FileUploadTeam {
+    public function answer(Team $team, int $fileUploadId, array $data): FileUploadTeam {
         $file = $data['file'];
+
+        $fileUpload = FileUpload::with('task')->findOrFail($fileUploadId);
+
         $task = $fileUpload->task;
 
         if ($team->tasks()->where('tasks.id', $task->id)->exists()) {
             throw new TaskAlreadyDoneException();
         }
 
-        \DB::beginTransaction();
+        DB::beginTransaction();
         $team->tasks()->attach($task->id);
 
         $fileUploadTeam = FileUploadTeam::create([
@@ -36,8 +41,11 @@ class FileUploadService
             'scorable_id' => $task->id,
             'scorable_type' => Task::class,
         ]);
+
+        /** @noinspection PhpUndefinedMethodInspection */
         $fileUploadTeam->addMedia($file);
-        \DB::commit();
+
+        DB::commit();
 
         return $fileUploadTeam;
     }
