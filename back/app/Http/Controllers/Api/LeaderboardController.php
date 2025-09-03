d88e56febcadd7ea8faa9d95e52ec187361f8e09
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-09-03 19:06:34 +0330

    feat: automatic filter date for leaderboard api

--- back/app/Http/Controllers/Api/LeaderboardController.php
+++ back/app/Http/Controllers/Api/LeaderboardController.php
@@ -4,21 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TeamResource;
-use App\Models\ScoreTeam;
 use App\Models\Team;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Modules\Support\Responses\ApiResponse;
 
 class LeaderboardController extends Controller
 {
     public function index()
     {
-        $teams = Team::orderBy('score', 'desc')
+        // all available dates
+        $dates = Team::select('start')
+            ->distinct()
+            ->orderBy('start')
+            ->pluck('start');
+
+        // last available date
+        $lastDate = Team::whereDate('start', '<=', now())->max('start');
+        
+        if (!$lastDate) {
+            return ApiResponse::success([
+                'dates' => $dates,
+                'last_date' => null,
+                'teams' => [],
+            ]);
+        }
+
+        // last available date teams
+        $teams = Team::whereDate('start', $lastDate)
+            ->orderBy('score', 'desc')
             ->orderBy('coin', 'desc')
-            ->limit(10)->get();
+            ->limit(10)
+            ->get();
 
         return ApiResponse::success([
+            'dates' => $dates,
+            'last_date' => $lastDate,
             'teams' => TeamResource::collection($teams),
         ]);
     }
