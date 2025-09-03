0f1e90b1b70253464c3d2412cad0ebb325fc43f5
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-09-03 19:28:43 +0330

    refactor: disable leaderboard api date filter

--- back/app/Http/Controllers/Api/LeaderboardController.php
+++ back/app/Http/Controllers/Api/LeaderboardController.php
@@ -11,33 +11,38 @@ class LeaderboardController extends Controller
 {
     public function index()
     {
-        // all available dates
-        $dates = Team::select('start')
-            ->distinct()
-            ->orderBy('start')
-            ->pluck('start');
+//        // all available dates
+//        $dates = Team::select('start')
+//            ->distinct()
+//            ->orderBy('start')
+//            ->pluck('start');
+//
+//        // last available date
+//        $lastDate = Team::whereDate('start', '<=', now())->max('start');
+//
+//        if (!$lastDate) {
+//            return ApiResponse::success([
+//                'dates' => $dates,
+//                'last_date' => null,
+//                'teams' => [],
+//            ]);
+//        }
+//
+//        // last available date teams
+//        $teams = Team::whereDate('start', $lastDate)
+//            ->orderBy('score', 'desc')
+//            ->orderBy('coin', 'desc')
+//            ->limit(10)
+//            ->get();
 
-        // last available date
-        $lastDate = Team::whereDate('start', '<=', now())->max('start');
-        
-        if (!$lastDate) {
-            return ApiResponse::success([
-                'dates' => $dates,
-                'last_date' => null,
-                'teams' => [],
-            ]);
-        }
-
-        // last available date teams
-        $teams = Team::whereDate('start', $lastDate)
-            ->orderBy('score', 'desc')
+        $teams = Team::orderBy('score', 'desc')
             ->orderBy('coin', 'desc')
             ->limit(10)
             ->get();
 
         return ApiResponse::success([
-            'dates' => $dates,
-            'last_date' => $lastDate,
+//            'dates' => $dates,
+//            'last_date' => $lastDate,
             'teams' => TeamResource::collection($teams),
         ]);
     }
