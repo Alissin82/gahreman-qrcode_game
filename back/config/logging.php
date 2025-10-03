75647d58be1bd9989c7d32c23efc6d58ed688fdd
Alissin82 <92631352+Alissin82@users.noreply.github.com>
2025-10-03 12:11:07 +0330

    Update logging.php

--- back/config/logging.php
+++ back/config/logging.php
@@ -32,8 +32,9 @@ return [
     */
 
     'deprecations' => [
-        'channel' => env('LOG_DEPRECATIONS_CHANNEL', 'null'),
-        'trace' => env('LOG_DEPRECATIONS_TRACE', false),
+        'driver' => 'single',
+        'path' => storage_path('logs/deprecations.log'),
+        'level' => 'notice',
     ],
 
     /*
